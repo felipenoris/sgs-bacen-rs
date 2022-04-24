6fef445f6a0cdf0e9e868add843aa619e50abb6e
Felipe Noronha <felipenoris@gmail.com>
2022-04-23 22:25:21 -0300

    split packages

--- sgslib/src/series.rs
+++ sgslib/src/series.rs
@@ -1,5 +1,6 @@
 use chrono::NaiveDate;
 use std::io::Read;
+use std::str::FromStr;
 use xml::reader::XmlEvent;
 use yaserde::YaDeserialize;
 use yaserde_derive::YaDeserialize;
@@ -36,7 +37,7 @@ impl YaDeserialize for ItemDate {
                 match res {
                     XmlEvent::Characters(str) => {
                         match NaiveDate::parse_from_str(&str, "%d/%m/%Y") {
-                            Ok(date) => Ok(ItemDate { date: date }),
+                            Ok(date) => Ok(ItemDate { date }),
                             _ => Err("Unexpected date format in `DATE` element.".to_string()),
                         }
                     }
@@ -80,9 +81,17 @@ pub struct Series {
     pub series: Vec<Serie>,
 }
 
+impl FromStr for Series {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        yaserde::de::from_str(s)
+    }
+}
+
 #[test]
 fn parse_series() {
     let response_string = "<?xml version='1.0' encoding='ISO-8859-1'?>\n<SERIES>\n<SERIE ID='1'>\n\t\t<ITEM>\n\t\t\t<DATA>31/3/2022</DATA>\n\t\t\t<VALOR>4.7378</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>1/4/2022</DATA>\n\t\t\t<VALOR>4.6984</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>4/4/2022</DATA>\n\t\t\t<VALOR>4.6175</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>5/4/2022</DATA>\n\t\t\t<VALOR>4.6400</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>6/4/2022</DATA>\n\t\t\t<VALOR>4.6967</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>7/4/2022</DATA>\n\t\t\t<VALOR>4.7422</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>8/4/2022</DATA>\n\t\t\t<VALOR>4.7513</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>11/4/2022</DATA>\n\t\t\t<VALOR>4.7025</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>12/4/2022</DATA>\n\t\t\t<VALOR>4.6483</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>13/4/2022</DATA>\n\t\t\t<VALOR>4.6811</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>14/4/2022</DATA>\n\t\t\t<VALOR>4.7158</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>18/4/2022</DATA>\n\t\t\t<VALOR>4.6746</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>19/4/2022</DATA>\n\t\t\t<VALOR>4.6664</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>20/4/2022</DATA>\n\t\t\t<VALOR>4.6397</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t\t<ITEM>\n\t\t\t<DATA>22/4/2022</DATA>\n\t\t\t<VALOR>4.7326</VALOR>\n\t\t\t<BLOQUEADO>false</BLOQUEADO>\n\t\t</ITEM>\n\t</SERIE>\n</SERIES>";
-    let series: Series = yaserde::de::from_str(response_string).unwrap();
+    let series = Series::from_str(response_string).unwrap();
     println!("{:?}", series);
 }
