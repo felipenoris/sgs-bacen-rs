eff716a855aa5a726ed96377518f1547327bb9da
Felipe Noronha <felipenoris@gmail.com>
2022-05-14 23:58:34 -0300

    lint

--- examples/julia_ffi_example/SGSJulia/test/runtests.jl
+++ examples/julia_ffi_example/SGSJulia/test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 import SGSJulia
 
 @testset "get_ultimo_valor_xml" begin
-	str = SGSJulia.get_ultimo_valor_xml(1)
-	@test startswith(str, "<?xml version")
+    str = SGSJulia.get_ultimo_valor_xml(1)
+    @test startswith(str, "<?xml version")
 end
